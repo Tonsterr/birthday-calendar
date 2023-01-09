5e95242fc7b1e2fa886c0689f2b696a23efa6686
Antonio Ndorimana <andorimana@gmail.com>
2023-01-08 21:41:39 -0500

    formatting cleanup using prettier and some refactoring

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Label from '@mui/material/InputLabel'
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Label from "@mui/material/InputLabel";
 
-interface Props{
-    search: string;
-    setSearch: React.Dispatch<React.SetStateAction<string>>
+interface Props {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
 }
-export default function SearchFiel({search,setSearch} : Props) {
-    return (
-        <div className="search">
-            <Label>Search</Label>
-            <TextField value={search} onChange={ (e) => setSearch(e.target.value)}/>
-        </div>
-    );
-  }
+export default function SearchFiel({ search, setSearch }: Props) {
+  return (
+    <div className="search">
+      <Label>Search</Label>
+      <TextField value={search} onChange={(e) => setSearch(e.target.value)} />
+    </div>
+  );
+}
